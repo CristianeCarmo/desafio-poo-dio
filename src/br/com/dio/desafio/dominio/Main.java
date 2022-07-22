59428e0c943fe170c613391388b6cd63e9aa0c6a
Cris <cristiane_scarmo@hotmail.com>
2022-07-22 10:51:32 -0300

    Final

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -19,9 +19,44 @@ public class Main {
         mentoria.setDescricao("Descricao mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCristiane = new Dev();
+        devCristiane.setNome("Cristiane");
+        devCristiane.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Cristiane: " + devCristiane.getConteudosInscritos());
+        devCristiane.progredir();
+        devCristiane.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Cristiane: " + devCristiane.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Cristiane: " + devCristiane.getConteudosConcluidos());
+        System.out.println("XP: " + devCristiane.calcularTotalXp());
+
+        System.out.println("--------");
+
+        Dev devLaura = new Dev();
+        devLaura.setNome("Laura");
+        devLaura.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Laura: " + devLaura.getConteudosInscritos());
+        devLaura.progredir();
+        devLaura.progredir();
+        devLaura.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Laura: " + devLaura.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Laura: " + devLaura.getConteudosConcluidos());
+        System.out.println("XP: " + devLaura.calcularTotalXp());
+
+
+
 
 
     }
